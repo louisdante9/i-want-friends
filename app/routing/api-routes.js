ef88d183fe2330d5821f7993c81b2cb0e717e24d
Michelle Luo <mluo0301@gmail.com>
2016-11-25 23:16:33 -0800

    adding readme

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,61 +1,92 @@
 // ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// We are linking our routes to a series of "data" sources. 
+// These data sources hold arrays of information on all possible friends
 // ===============================================================================
 
-var tableData 		= require('../data/table-data.js');
-var waitListData 	= require('../data/waitinglist-data.js');
+var friends 		= require('../data/friends.js');
 
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function (app) {
+module.exports = function(app){
+
 	// API GET Requests
-	// Below code handles when users "visit" a page.
-	// In each of the below cases when a user visits a link
-	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+	// Below code handles when users "visit" a page. 
+	// In each of the below cases when a user visits a link 
+	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table) 
 	// ---------------------------------------------------------------------------
 
-	app.get('/api/tables', function (req, res) {
-		res.json(tableData);  //tables's tabledatadisplay.
+	app.get('/api/friends', function(req, res){
+		res.json(friends);
 	});
 
-	app.get('/api/waitlist', function (req, res) {
-		res.json(waitListData); //tables's waitinglist display.
-	});
 
 	// API POST Requests
 	// Below code handles when a user submits a form and thus submits data to the server.
 	// In each of the below cases, when a user submits form data (a JSON object)
 	// ...the JSON is pushed to the appropriate Javascript array
-	// (ex. User fills out a reservation request... this data is then sent to the server...
-	// Then the server saves the data to the tableData array)
 	// ---------------------------------------------------------------------------
 
-	app.post('/api/tables', function (req, res) {
-		// Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-		// It will do this by sending out the value "true" have a table
-		if (tableData.length < 5) {
-			tableData.push(req.body);    //????
-			res.json(true); // KEY LINE
-		} else { // Or false if they don't have a table
-			waitListData.push(req.body);
-			res.json(false); // KEY LINE
+	app.post('/api/friends', function(req, res){
+
+		// Note the code here. Our "server" will respond to a user's survey result
+		// Then compare those results against every user in the database.
+		// It will then calculate the difference between each of the numbers and the user's numbers.
+		// It will then choose the user with the least differences as the "best friend match."
+		// In the case of multiple users with the same result it will choose the first match.
+		// After the test, it will push the user to the database. 
+
+		// We will use this object to hold the "best match". We will constantly update it as we 
+		// loop through all of the options 
+		var bestMatch = {
+			name: "",
+			photo: "",
+			friendDifference: 1000
+		};
+
+		// Here we take the result of the user's survey POST and parse it.
+		var userData 	= req.body;
+		var userName 	= userData.name;
+		var userPhoto 	= userData.photo;
+		var userScores 	= userData.scores;
+
+		// This variable will calculate the difference between the user's scores and the scores of
+		// each user in the database
+		var totalDifference = 0;
+
+		// Here we loop through all the friend possibilities in the database. 
+		for  (var i=0; i< friends.length; i++) {
+
+			console.log(friends[i].name);
+			totalDifference = 0;
+
+			// We then loop through all the scores of each friend
+			for (var j=0; j< friends[i].scores[j]; j++){
+
+				// We calculate the difference between the scores and sum them into the totalDifference
+				totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+				// If the sum of differences is less then the differences of the current "best match"
+				if (totalDifference <= bestMatch.friendDifference){
+
+					// Reset the bestMatch to be the new friend. 
+					bestMatch.name = friends[i].name;
+					bestMatch.photo = friends[i].photo;
+					bestMatch.friendDifference = totalDifference;
+				}
+			}
 		}
+
+		// Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
+		// the database will always return that the user is the user's best friend).
+		friends.push(userData);
+
+		// Return a JSON with the user's bestMatch. This will be used by the HTML in the next page. 
+		res.json(bestMatch);
+
 	});
 
-	// ---------------------------------------------------------------------------
-	// I added this below code so you could clear out the table while working with the functionality.
-	// Don't worry about it!
-
-	app.post('/api/clear', function () {
-		// Empty out the arrays of data
-		tableData = [];
-		waitListData = [];
-
-		console.log(tableData);
-	});
-};
+}
--- server.js
+++ server.js
@@ -1,28 +1,59 @@
-//npm package
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
 
-//define express & define PORT
-var app = express ();// Tells node that we are creating an "express" server
-var PORT = process.env.PORT || 9999;
 
-// body Parser standards. BodyParser makes it easy for our server to interpret data sent to it.
+
+
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server 
+// ==============================================================================
+
+var app = express(); // Tells node that we are creating an "express" server
+var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
+
+// BodyParser makes it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-//ROUTER...
-//These routes give our server a "map" of how to respond when users visit or request data from various URLs. 
+
+
+
+
+
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs. 
+// ================================================================================
+
 require('./app/routing/api-routes.js')(app); 
 require('./app/routing/html-routes.js')(app);
 
-//Listener
-//The below code effectively "starts" our server 
 
-app.listen(PORT,function(){
-	console.log("app listening on PORT: " + PORT);
 
-})
+
+
+
+
+
+// ==============================================================================
+// LISTENER
+// The below code effectively "starts" our server 
+// ==============================================================================
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
